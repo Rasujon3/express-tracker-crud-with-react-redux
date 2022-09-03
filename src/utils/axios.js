cef93c5150570cb3f899ee4e3d15b1e298f52f6d
Rasujon3 <rasujon3@gmail.com>
2022-09-04 01:30:31 +0600

    7.3 Create APIs

---
+++ src/utils/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://sujonlwsserver.herokuapp.com",
+  //   baseURL: "http://localhost:9000",
+});
+
+export default axiosInstance;
