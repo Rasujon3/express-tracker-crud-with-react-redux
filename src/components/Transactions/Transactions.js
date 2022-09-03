c9ee7c1882ce9a8a746b34acb32f7a75c2fc8a73
Rasujon3 <rasujon3@gmail.com>
2022-09-04 03:33:09 +0600

    7.6 List Transaction

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Transaction from "./Transaction";
 
 const Transactions = () => {
+  const { transactions, isLoading, isError, error } = useSelector(
+    (state) => state.transaction
+  );
+
+  //   decide what to render
+  let content = null;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (!isLoading && isError) {
+    content = <p className="error">There was an error occured</p>;
+  }
+
+  if (!isLoading && !isError && transactions?.length > 0) {
+    content = transactions.map((transaction) => (
+      <Transaction key={transaction.id} transaction={transaction} />
+    ));
+  }
+
+  if (!isLoading && !isError && transactions?.length === 0) {
+    content = <p>No transactions found!</p>;
+  }
   return (
     <>
       <p className="second_heading">Your Transactions:</p>
@@ -9,7 +33,7 @@ const Transactions = () => {
       <div className="conatiner_of_list_of_transactions">
         <ul>
           {/* single Transaction */}
-          <Transaction />
+          {content}
         </ul>
       </div>
     </>
